feb8dab3c94f420398d606460d09528e6621d13e
sys-35 <sys-35@sys-49-PC>
2018-02-16 14:56:37 +0530

    new commit

--- Basics/src/practices/Singleton.java
+++ Basics/src/practices/Singleton.java
@@ -22,7 +22,7 @@ public class Singleton {
 		  System.out.println("Please Say Hai");
 	  }
 	  
-	  public static void main(String []args)
+	  public static void main(String []sarath)
 	  {
 		  Singleton sa=Singleton.getInstance();
 		  sa.hai();
