985e833648e24299d1fe48a0fbbf4ddc1ea088e5
sys-35 <sys-35@sys-49-PC>
2018-02-19 11:04:19 +0530

    commit

---
+++ Basics/src/privateParent/PrivateParent.java
@@ -0,0 +1,10 @@
+package privateParent;
+
+public class PrivateParent {
+	
+	void m1()
+	{
+		System.out.println("Hai Sarath");
+	}
+
+}
---
+++ Basics/src/privateParent/child.java
@@ -0,0 +1,11 @@
+package privateParent;
+
+public class child extends PrivateParent {
+	
+	public static void main(String []args)
+	{
+		child c1=new child();
+		c1.m1();
+	}
+
+}
