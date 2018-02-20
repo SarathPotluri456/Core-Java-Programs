2bdf81aa097e01fca3d1fbb313bce192c99abb9c
sys-35 <sys-35@sys-49-PC>
2018-02-20 12:25:12 +0530

    commit

--- Basics/src/shallowAndDeepCopies/CopyConstructor.java
+++ Basics/src/shallowAndDeepCopies/CopyConstructor.java
@@ -15,7 +15,7 @@ public class CopyConstructor {
 		
 	}
 	
-	public static void main(String []args)
+	public  static void main(String []args)
 	{
 		CopyConstructor c1=new CopyConstructor(20,30);
 		CopyConstructor c2=new CopyConstructor(c1);
